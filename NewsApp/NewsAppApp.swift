5e1d9c75881ceed550819f87dfa191dc2380fb50
Tanirbergen Kaldibai <tanirbergen.kaldybay@airba.kz>
2022-07-12 15:58:36 +0600

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Didar Bakhitzhanov on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
